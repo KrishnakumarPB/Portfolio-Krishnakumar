092b66b573406586a68966d4434e9b3cbc6351a6
Krishnakumar <krishnakumar.amritham@gmail.com>
2021-08-20 15:51:55 +0530

    Animations in description

--- src/Projectcard.js
+++ src/Projectcard.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./projects.css"
+let currentelement=""
+function showDesc(uid){
+    console.log("hovered");
+currentelement=document.getElementById(uid);
+currentelement.querySelector(".app-description").classList.remove("hidden");
+currentelement.querySelector(".app-description").classList.add("app-description-active");
+currentelement.querySelector(".app-description").style.animation="descmove .5s ease-out"
+}
+function hideDesc(uid){
+currentelement=document.getElementById(uid);
+currentelement.querySelector(".app-description").classList.add("hidden");
+currentelement.querySelector(".app-description").classList.remove("app-description-active");
+}
 function Projectcard (props){
     return(
         <div>
-           <div className="card-container">
+            
+           <div className="card-container" onMouseOver={()=>showDesc(props.uniqueid)} onMouseOut={()=>hideDesc(props.uniqueid)} id={props.uniqueid} >
+               <div className="app-description hidden"><p>{props.description}</p></div>
                <img src={props.imgurl} alt="img"/>
                <a class=" live-btn" href={props.liveurl} target="_blank" rel="noreferrer">
                     View Live
--- src/Projects.js
+++ src/Projects.js
@@ -14,27 +14,39 @@ function Projects (){
         <Projectcard
         imgurl={Tictactoe}
         liveurl="#"
-        title="Tic Tac Toe"/>
+        title="Tic Tac Toe"
+        description="A 2-player game completely made in React .Player tries two vertical lines crossing two horizontal lines and each tries to get a row of three Xs or three Os before the opponent does."
+        uniqueid="100"/>
         <Projectcard
         imgurl={Moviereview}
         liveurl="https://react-jhdixq.stackblitz.io"
-        title="Movie Review"/>
+        title="Movie Review"
+        description="A movie information center which the user gets some famous movie's name title and its release date along with plot"
+        uniqueid="101"/>
         <Projectcard
         imgurl={Memegenerator}
         liveurl="https://meme-generator-live.netlify.app/"
-        title="Meme Generator"/>
+        title="Meme Generator"
+        description="Random collection of famous meme photographs which will allow user to enter texts to generate a new meme.On clicking New Image button the meme image will change and user can choose appropriate image for their idea"
+        uniqueid="102"/>
         <Projectcard
         imgurl={Todos}
         liveurl="https://todo-list-live.netlify.app/"
-        title="Todo List"/>
+        title="Todo List"
+        description="A comprehensive task input page where the user can manage the task along with option of marking the task as completed"
+        uniqueid="103"/>
         <Projectcard
         imgurl={Drums}
-        liveurl="#"
-        title="Drumpad"/>
+        liveurl="https://drumkit-live.netlify.app/"
+        title="Drumpad"
+        description="Certain set of drum pad elements which can be used for the user to play the score"
+        uniqueid="104"/>
         <Projectcard
         imgurl={Quotecenter}
         liveurl="https://react-khde5v.stackblitz.io/"
-        title="Quote Center"/>
+        title="Quote Center"
+        description="A static wesite of uotes of famous successful athletes along with their picture and name."
+        uniqueid="105"/>
         </div>
 </div>
     )
